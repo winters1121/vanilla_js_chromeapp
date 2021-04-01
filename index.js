8ce87d42ce3cc981320ee1bf22740cdefbde9810
winters1121 <wuk1121@gmail.com>
2021-04-01 22:04:38 +0900

    #1.10 Organizing data with Objects

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const thing = "thing";
+const country = {
+  one: "Republic of Korea",
+  two: "Afreeca",
+  three: "United States",
+};
+console.log(country);
 
-const arrayOfData = ["haha", "jaja", thing, 15000, 1.5, true, false];
+country.two = "Canada";
+console.log(country);
 
-console.log(arrayOfData);
-console.log(arrayOfData[1]);
+const swInfo = {
+  name: "SangWook",
+  age: 28,
+  city: "Seoul",
+  fatty: false,
+  favMovies: ["BigFish", "HarryPorter", "Soul"],
+  favFoods: [
+    { name: "Chicken", protein: true },
+    { name: "Sashimi", protein: true },
+  ],
+};
+
+console.log(swInfo);
