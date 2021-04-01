178f064accbacda24aaed1c283cb4e6371040964
winters1121 <wuk1121@gmail.com>
2021-04-01 20:32:19 +0900

    #1.8 Data Types(String, Boolean, Number, Float)

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-let a = 33;
-let b = a - 16;
-a = 36;
-console.log(b, a);
+/* String
+const feeling = "행복";
+const possible = "0123456789";
+console.log(feeling, possible); */
+
+/* Boolean
+const progressive = true;
+const conservative = false;
+console.log(progressive); */
+
+/* Number
+const phonenumber = 17536;
+console.log(phonenumber); */
+
+/* Float
+const myeyesight = 1.2;
+console.log(myeyesight); */
