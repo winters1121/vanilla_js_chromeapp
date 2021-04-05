a295f2f51d38f8eda66058cd8104931b5c93ab03
winters1121 <wuk1121@gmail.com>
2021-04-05 16:41:28 +0900

    #2.1 done

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-function myDear(dear, chicken) {
-  return `Hello ${dear}, you are ${chicken} years old`;
-}
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  multiple: function (a, b) {
+    return a * b;
+  },
+  division: function (a, b) {
+    return a / b;
+  },
+  square: function (a, b) {
+    return a ** b;
+  },
+};
 
-const greetSw = myDear("Nakyeong", 22);
-
-console.log(greetSw);
+// console.log(greetSw);
+const plus = calculator.plus(10, 10);
+console.log(plus);
+const minus = calculator.minus(10, 5);
+console.log(minus);
+const multiple = calculator.multiple(10, 1000);
+console.log(multiple);
+const division = calculator.division(1000, 10);
+console.log(division);
+const square = calculator.square(100, 10);
+console.log(square);
