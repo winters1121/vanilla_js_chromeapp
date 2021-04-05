8099c0ece4e7d43ce373170e99972446829645fe
winters1121 <wuk1121@gmail.com>
2021-04-05 17:34:10 +0900

    #2.2 DOM with JS

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  multiple: function (a, b) {
-    return a * b;
-  },
-  division: function (a, b) {
-    return a / b;
-  },
-  square: function (a, b) {
-    return a ** b;
-  },
-};
+const title = document.getElementById("title");
 
-// console.log(greetSw);
-const plus = calculator.plus(10, 10);
-console.log(plus);
-const minus = calculator.minus(10, 5);
-console.log(minus);
-const multiple = calculator.multiple(10, 1000);
-console.log(multiple);
-const division = calculator.division(1000, 10);
-console.log(division);
-const square = calculator.square(100, 10);
-console.log(square);
+title.innerHTML = "Hello~ My Dear!";
+title.style.color = "blue";
+document.title = "I love u.";
