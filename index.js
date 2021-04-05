3814bbd136a80cb99be5e761b9ec377c464bd15c
winters1121 <wuk1121@gmail.com>
2021-04-05 16:11:45 +0900

    ` ` use it.

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 function myDear(dear, chicken) {
-  console.log(
-    "hello, my Dear.",
-    dear,
-    "I'm so much want to see you.",
-    "so",
-    chicken,
-    "day after we'll meet. yea!"
-  );
+  console.log(`Hello ${dear}, you are ${chicken} years old`);
 }
-myDear("Nakyeong", 1);
+
+myDear("Nakyeong", 22);
